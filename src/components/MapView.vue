37b7a7e663444bc70dbd45aac22247ba870e53c6
Indhu.k <kumar.swami02021970@gmail.com>
2025-06-12 22:33:17 +0530

    commit changes

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,14 +1,25 @@
-<template>
-  <div id="map" style="height: 400px;"></div>
-</template>
-
 <script>
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
 export default {
   props: ['stations'],
   mounted() {
+    // Fix default marker icon path
+    const DefaultIcon = L.icon({
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
+
+    // Create map
     this.map = L.map('map').setView([20, 78], 4);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
@@ -21,7 +32,7 @@ export default {
   },
   methods: {
     renderMarkers() {
-      if (!this.map) return;
+      if (!this.map || !this.stations) return;
       this.stations.forEach(station => {
         L.marker([station.location.lat, station.location.lng])
           .addTo(this.map)
